b85a866d583f1bd9985ed361aeebde1c4115f96c
cristinazuhe <zuhe18@gmail.com>
2024-01-11 15:13:23 +0100

    Modified the .py to train the model.

--- flexnlp/notebooks/Federated_QA.py
+++ flexnlp/notebooks/Federated_QA.py
@@ -2,6 +2,7 @@ from copy import deepcopy
 import torch
 import torch.nn as nn
 from datasets import load_dataset
+from datasets import Dataset as HFDataset
 from transformers import AutoTokenizer
 from transformers import DefaultDataCollator
 from transformers import AutoModelForQuestionAnswering, TrainingArguments, Trainer
@@ -42,7 +43,7 @@ model_checkpoint = "distilbert-base-uncased"
 #model_checkpoint = "bert-base-cased"
 tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
 
-max_length = 384
+max_length = 512
 stride = 128
 
 
@@ -101,6 +102,73 @@ def preprocess_training_examples(examples):
     inputs["end_positions"] = end_positions
     return inputs
 
+def preprocess_training_examples_as_lists(examples, answers_examples):
+    """
+    Function that preprocess the data that comes as a list 
+    instead as a Dataset type.
+    Args:
+        examples (list[list]): List of lists containg the examples to
+        preprocess. ['id', 'title', 'context', 'question']
+        answers (list[str]): List containing the answers
+    """
+    questions = [q[3].strip() for q in examples]
+    contexts = [c[2] for c in examples]
+    inputs = tokenizer(
+        questions,
+        # examples["context"],
+        contexts,
+        max_length=max_length,
+        truncation="only_second",
+        stride=stride,
+        return_overflowing_tokens=True,
+        return_offsets_mapping=True,
+        padding="max_length",
+    )
+
+    offset_mapping = inputs.pop("offset_mapping")
+    sample_map = inputs.pop("overflow_to_sample_mapping")
+    # answers = examples["answers"]
+    answers = [answers_examples[1][i] for i in range(len(answers_examples[1]))]
+    start_positions = []
+    end_positions = []
+
+    for i, offset in enumerate(offset_mapping):
+        sample_idx = sample_map[i]
+        answer = answers[sample_idx]
+        start_char = answer["answer_start"][0]
+        end_char = answer["answer_start"][0] + len(answer["text"][0])
+        sequence_ids = inputs.sequence_ids(i)
+
+        # Find the start and end of the context
+        idx = 0
+        while sequence_ids[idx] != 1:
+            idx += 1
+        context_start = idx
+        while sequence_ids[idx] == 1:
+            idx += 1
+        context_end = idx - 1
+
+        # If the answer is not fully inside the context, label is (0, 0)
+        if offset[context_start][0] > start_char or offset[context_end][1] < end_char:
+            start_positions.append(0)
+            end_positions.append(0)
+        else:
+            # Otherwise it's the start and end token positions
+            idx = context_start
+            while idx <= context_end and offset[idx][0] <= start_char:
+                idx += 1
+            start_positions.append(idx - 1)
+
+            idx = context_end
+            while idx >= context_start and offset[idx][1] >= end_char:
+                idx -= 1
+            end_positions.append(idx + 1)
+
+    inputs["start_positions"] = start_positions
+    inputs["end_positions"] = end_positions
+    return HFDataset.from_dict(inputs)
+
+
 def preprocess_validation_examples(examples):
     questions = [q.strip() for q in examples["question"]]
     inputs = tokenizer(
@@ -130,11 +198,15 @@ def preprocess_validation_examples(examples):
     inputs["example_id"] = example_ids
     return inputs
 
-train_dataset = squad["train"].map(
+
+train_dataset_processed = squad["train"].map(
     preprocess_training_examples,
     batched=True,
     remove_columns=squad["train"].column_names,
 )
+
+
+train_dataset = squad["train"]
 
 test_dataset = squad["test"].map(
     preprocess_validation_examples,
@@ -148,9 +220,12 @@ config = FedDatasetConfig(seed=0)
 config.n_clients = 2
 config.replacement = False # ensure that clients do not share any data
 config.client_names = ['client1', 'client2'] # Optional
-flex_dataset = FedDataDistribution.from_config_with_huggingface_dataset(data=train_dataset, config=config, 
-                                                                        X_columns=['input_ids', 'attention_mask'],
-                                                                        label_columns=['start_positions', 'end_positions'])
+flex_dataset = FedDataDistribution.from_config_with_huggingface_dataset(data=train_dataset, config=config,
+                                                                        X_columns=['id', 'title', 'context', 'question'],
+                                                                        label_columns=['answers']
+                                                                        # X_columns=['input_ids', 'attention_mask'],
+                                                                        # label_columns=['start_positions', 'end_positions']
+                                                                        )
 
 # Adapt the test dataset to a FlexDataset
 test_dataset = Dataset.from_huggingface_dataset(test_dataset,
@@ -202,10 +277,14 @@ class QADataset(TorchDataset):
 
 # Train each client's model
 def train(client_flex_model: FlexModel, client_data: Dataset):
+    print("Training client")
     model = client_flex_model['model']
     training_args = client_flex_model['training_args']
-    client_train_dataset = client_data.to_numpy()
-    print(client_data)
+    # client_train_dataset = client_data.to_numpy()
+    X_data = client_data.X_data.tolist()
+    y_data = client_data.to_list()
+    client_train_dataset = preprocess_training_examples_as_lists(examples=X_data, answers_examples=y_data)
+    # breakpoint()
     trainer = Trainer(
         model = model,
         args=training_args,
